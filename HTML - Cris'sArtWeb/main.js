28cfb221727d35cbd47e06f6189b710c6c5e5571
cheeze <anhhl2@uci.edu>
2022-02-20 12:55:27 -0800

    updated cristine website + fixed carousel

--- HTML - Cris'sArtWeb/main.js
+++ HTML - Cris'sArtWeb/main.js
@@ -1,1 +1,13 @@
 
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  items: 1,
+  slideSpeed : 500,
+  // paginationSpeed : 400,
+  // singleItem : true,
+  // autoHeight : true,
+  // transitionStyle:"fade",
+});
